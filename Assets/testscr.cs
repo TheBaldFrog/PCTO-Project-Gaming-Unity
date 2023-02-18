6f1bc28a923a1924d653f9c64d61faea9a0340ce
TheBaldFrog <morarolivio@gmail.com>
2023-02-18 01:07:00 +0100

    add cont a

--- Assets/testscr.cs
+++ Assets/testscr.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class scr : MonoBehaviour
 {
+    int a = 0;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        a++;
     }
 
     // Update is called once per frame
